5b8951e660dc706183ad8e099ba03a2a12f3e79d
keholsa <keenan.holsapple@okstate.edu>
2023-04-08 14:53:55 -0500

    part1 complete, part2 and part3 need work

---
+++ part3.py
@@ -0,0 +1,4 @@
+# https://keras.io/api/datasets/cifar10/
+# create CNN with cifar10 datasheet and comapre with 
+# https://www.tensorflow.org/api_docs/python/tf/keras/applications/resnet50/ResNet50
+# resnet50 complete dataset minus the last layer then add layers based on compiled dataset in comparsion
